48282906375353f9b146be01db3973d83c31b0fc
Saribeg <saribeg@gmail.com>
2025-06-11 21:02:47 +0400

    part-1/task-1.4: add: Course Information, step 4

--- Part-1/courseinfo/src/App.jsx
+++ Part-1/courseinfo/src/App.jsx
@@ -4,24 +4,26 @@ import Total from './Total'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content content={[part1, part2, part3]} />
-      <Total numberOfExercises={[part1, part2, part3].reduce((sum, item )=> sum + item.exercises, 0)}/>
+      <Content content={parts} />
+      <Total numberOfExercises={parts.reduce((sum, item )=> sum + item.exercises, 0)}/>
     </div>
   )
 }
