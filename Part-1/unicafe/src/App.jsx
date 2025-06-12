f7b750ee3f9a5aba91d74d4f989e208c9625c698
Saribeg <saribeg@gmail.com>
2025-06-12 19:35:05 +0400

    part-1/task-1.11: add: unicafe, step 6, Table

--- Part-1/unicafe/src/App.jsx
+++ Part-1/unicafe/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div>
       <h1>Give Feedback</h1>
-      <div>
+      <div style={{ marginBottom: '20px' }}>
         <Button handleClick={() => setGood(good + 1)} text="Good"/>
         <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
         <Button handleClick={() => setBad(bad + 1)} text="Bad"/>
