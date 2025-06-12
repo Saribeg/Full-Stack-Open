82d0681b4c6933a64b1f51806702a48ae13b2be6
Saribeg <saribeg@gmail.com>
2025-06-12 18:51:30 +0400

    part-1/task-1.9: add: unicafe, step 4

--- Part-1/unicafe/src/Statistics.jsx
+++ Part-1/unicafe/src/Statistics.jsx
@@ -3,6 +3,10 @@ const Statistics = ({statistics: { good, neutral, bad }}) => {
   const average = total === 0 ? 0 : (good - bad) / total
   const positive = total === 0 ? 0 : (good / total) * 100
 
+  if (!total) {
+    return <p>No feedback given</p>
+  }
+
   return (
     <div>
       <p>Good: {good}</p>
