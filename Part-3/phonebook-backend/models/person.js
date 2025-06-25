34f56b7c5d6f2ced1c8df3b019fcd2587fbc7b05
Saribeg <saribeg@gmail.com>
2025-06-25 21:36:22 +0400

    part-3/task-3.20: add: Phonebook database, step 8

--- Part-3/phonebook-backend/models/person.js
+++ Part-3/phonebook-backend/models/person.js
@@ -8,7 +8,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
   },
 })
 
