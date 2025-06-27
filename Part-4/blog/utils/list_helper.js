92814f5e2f9a78155ad5b24d634fac9680740c18
Saribeg <saribeg@gmail.com>
2025-06-27 14:25:11 +0400

    part-4/task-4.4: add: Helper Functions and Unit Tests, step 2

--- Part-4/blog/utils/list_helper.js
+++ Part-4/blog/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = () => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (!Array.isArray(blogs) || !blogs.length) {
+    return 0;
+  }
+
+  return blogs.reduce((acc, blog) => acc += blog.likes, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
