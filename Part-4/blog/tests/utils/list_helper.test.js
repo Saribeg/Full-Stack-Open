817e81fa1f5217dfeaf7752ef8ed07341c00ccbd
Saribeg <saribeg@gmail.com>
2025-06-28 20:31:24 +0400

    part-4/task-4.14: add: Blog List Expansions, step 2

--- Part-4/blog/tests/utils/list_helper.test.js
+++ Part-4/blog/tests/utils/list_helper.test.js
@@ -53,116 +53,118 @@ const blogs = [
   }
 ];
 
-test('dummy returns one', () => {
-  const blogs = [];
+describe('Unit tests', () => {
+  test('dummy returns one', () => {
+    const blogs = [];
 
-  const result = listHelper.dummy(blogs);
-  assert.strictEqual(result, 1);
-});
-
-describe('total likes', () => {
-  test('when no array passed the result is 0', () => {
-    const result = listHelper.totalLikes('string');
-    assert.strictEqual(result, 0);
+    const result = listHelper.dummy(blogs);
+    assert.strictEqual(result, 1);
   });
 
-  test('of empty list is 0', () => {
-    const result = listHelper.totalLikes([]);
-    assert.strictEqual(result, 0);
-  });
-
-  test('when list has only one blog equals the like of that', () => {
-    const result = listHelper.totalLikes([blogs[0]]);
-    assert.strictEqual(result, 7);
-  });
-
-  test('of a bigger list is calculated right', () => {
-    const result = listHelper.totalLikes(blogs);
-    assert.strictEqual(result, 36);
-  });
-});
-
-describe('favourite blog', () => {
-  test('when no array passed the result is null', () => {
-    const result = listHelper.favoriteBlog('string');
-    assert.strictEqual(result, null);
-  });
-
-  test('of empty list is null', () => {
-    const result = listHelper.favoriteBlog([]);
-    assert.strictEqual(result, null);
-  });
-
-  test('when list has only one blog equals that', () => {
-    const result = listHelper.favoriteBlog([blogs[0]]);
-    assert.deepStrictEqual (result, blogs[0]);
-  });
-
-  test('of a bigger list is found right', () => {
-    const result = listHelper.favoriteBlog(blogs);
-    assert.deepStrictEqual(result,   {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 12,
-      __v: 0
+  describe('total likes', () => {
+    test('when no array passed the result is 0', () => {
+      const result = listHelper.totalLikes('string');
+      assert.strictEqual(result, 0);
     });
-  });
-});
 
-describe('authors with most blogs', () => {
-  test('when no array passed the result is null', () => {
-    const result = listHelper.mostBlogs('string');
-    assert.strictEqual(result, null);
-  });
+    test('of empty list is 0', () => {
+      const result = listHelper.totalLikes([]);
+      assert.strictEqual(result, 0);
+    });
 
-  test('when empty list the result is null', () => {
-    const result = listHelper.mostBlogs([]);
-    assert.strictEqual(result, null);
-  });
+    test('when list has only one blog equals the like of that', () => {
+      const result = listHelper.totalLikes([blogs[0]]);
+      assert.strictEqual(result, 7);
+    });
 
-  test('when list has only one blog equals that author', () => {
-    const result = listHelper.mostBlogs([blogs[0]]);
-    assert.deepStrictEqual (result, {
-      author: 'Michael Chan',
-      blogs: 1
+    test('of a bigger list is calculated right', () => {
+      const result = listHelper.totalLikes(blogs);
+      assert.strictEqual(result, 36);
     });
   });
 
-  test('when more than 1 author calculates right', () => {
-    const result = listHelper.mostBlogs(blogs);
-    assert.deepStrictEqual(result,   {
-      author: 'Robert C. Martin',
-      blogs: 3
+  describe('favourite blog', () => {
+    test('when no array passed the result is null', () => {
+      const result = listHelper.favoriteBlog('string');
+      assert.strictEqual(result, null);
+    });
+
+    test('of empty list is null', () => {
+      const result = listHelper.favoriteBlog([]);
+      assert.strictEqual(result, null);
+    });
+
+    test('when list has only one blog equals that', () => {
+      const result = listHelper.favoriteBlog([blogs[0]]);
+      assert.deepStrictEqual (result, blogs[0]);
+    });
+
+    test('of a bigger list is found right', () => {
+      const result = listHelper.favoriteBlog(blogs);
+      assert.deepStrictEqual(result,   {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0
+      });
     });
   });
-});
 
-describe('authors with most likes', () => {
-  test('when no array passed the result is null', () => {
-    const result = listHelper.mostLikes('string');
-    assert.strictEqual(result, null);
-  });
+  describe('authors with most blogs', () => {
+    test('when no array passed the result is null', () => {
+      const result = listHelper.mostBlogs('string');
+      assert.strictEqual(result, null);
+    });
 
-  test('when empty list the result is null', () => {
-    const result = listHelper.mostLikes([]);
-    assert.strictEqual(result, null);
-  });
+    test('when empty list the result is null', () => {
+      const result = listHelper.mostBlogs([]);
+      assert.strictEqual(result, null);
+    });
 
-  test('when list has only one blog equals that author', () => {
-    const result = listHelper.mostLikes([blogs[0]]);
-    assert.deepStrictEqual (result, {
-      author: 'Michael Chan',
-      likes: 7
+    test('when list has only one blog equals that author', () => {
+      const result = listHelper.mostBlogs([blogs[0]]);
+      assert.deepStrictEqual (result, {
+        author: 'Michael Chan',
+        blogs: 1
+      });
+    });
+
+    test('when more than 1 author calculates right', () => {
+      const result = listHelper.mostBlogs(blogs);
+      assert.deepStrictEqual(result,   {
+        author: 'Robert C. Martin',
+        blogs: 3
+      });
     });
   });
 
-  test('when more than 1 author calculates right', () => {
-    const result = listHelper.mostLikes(blogs);
-    assert.deepStrictEqual(result,   {
-      author: 'Edsger W. Dijkstra',
-      likes: 17
+  describe('authors with most likes', () => {
+    test('when no array passed the result is null', () => {
+      const result = listHelper.mostLikes('string');
+      assert.strictEqual(result, null);
+    });
+
+    test('when empty list the result is null', () => {
+      const result = listHelper.mostLikes([]);
+      assert.strictEqual(result, null);
+    });
+
+    test('when list has only one blog equals that author', () => {
+      const result = listHelper.mostLikes([blogs[0]]);
+      assert.deepStrictEqual (result, {
+        author: 'Michael Chan',
+        likes: 7
+      });
+    });
+
+    test('when more than 1 author calculates right', () => {
+      const result = listHelper.mostLikes(blogs);
+      assert.deepStrictEqual(result,   {
+        author: 'Edsger W. Dijkstra',
+        likes: 17
+      });
     });
   });
 });
