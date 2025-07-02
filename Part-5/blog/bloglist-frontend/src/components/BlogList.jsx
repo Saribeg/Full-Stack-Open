9fdd229e269c2140f7df80541b6ae8641b505c38
Saribeg <saribeg@gmail.com>
2025-07-02 17:23:56 +0400

    part-5/task-5.8: add: Blog List Frontend, step 8

--- Part-5/blog/bloglist-frontend/src/components/BlogList.jsx
+++ Part-5/blog/bloglist-frontend/src/components/BlogList.jsx
@@ -1,11 +1,11 @@
 import Blog from './Blog/Blog';
 
-const BlogList = ({ blogs }) => {
+const BlogList = ({ blogs, updateBlog, notify }) => {
   return (
     <div>
       <h2>Blogs</h2>
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} notify={notify}/>
       )}
     </div>
   );
