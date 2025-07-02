3a8a71ac2f4f395d71883df0c7f74b93579711f9
Saribeg <saribeg@gmail.com>
2025-07-03 00:12:36 +0400

    part-5/task-5.13: add: Blog List Tests, step 1

--- Part-5/blog/bloglist-frontend/vite.config.js
+++ Part-5/blog/bloglist-frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     proxy: {
       '/api': {
@@ -12,4 +22,10 @@ export default defineConfig({
       },
     },
   },
+  test: {
+    include: ['tests/**/*.test.jsx'],
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './testSetup.js',
+  }
 });
