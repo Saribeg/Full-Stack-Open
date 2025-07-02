d4c9dee6ac96adea80cd4936aa720418d87df45d
Saribeg <saribeg@gmail.com>
2025-07-02 18:48:19 +0400

    part-5/task-5.11: add: Blog List Frontend, step 11

--- Part-5/blog/bloglist-frontend/src/components/Blog/Blog.jsx
+++ Part-5/blog/bloglist-frontend/src/components/Blog/Blog.jsx
@@ -2,15 +2,31 @@ import { useState } from 'react';
 import blogService from '../../services/blogs';
 import './blog.css';
 
-const Blog = ({ blog, updateBlog, notify }) => {
+const Blog = ({ blog, user, modifyBlogs, notify }) => {
   const [expanded, setExpanded] = useState(false);
 
   const handleBlogUpdate = async () => {
     try {
       const updatedBlog = await blogService.update({ ...blog, likes: blog.likes + 1 });
-      updateBlog(updatedBlog);
+      modifyBlogs('update', updatedBlog);
       notify({
         message: `Blog ${updatedBlog.title} is successfully updated`,
+        type: 'success'
+      });
+    } catch(error){
+      notify({
+        message: error.message,
+        type: 'error'
+      });
+    }
+  };
+
+  const handleBlogDelete = async () => {
+    try {
+      await blogService.deleteBlog(blog.id);
+      modifyBlogs('delete', { id: blog.id });
+      notify({
+        message: `Blog ${blog.title} is successfully deleted`,
         type: 'success'
       });
     } catch(error){
@@ -38,6 +54,7 @@ const Blog = ({ blog, updateBlog, notify }) => {
             <button className='blog-like' onClick={handleBlogUpdate}>❤️ Like</button>
           </div>
           {blog.user?.name && <div>User: {blog.user.name}</div>}
+          {user?.id === blog.user?.id ? <button onClick={handleBlogDelete}>Delete</button> : null}
         </div>
       )}
     </div>
--- Part-5/blog/bloglist-frontend/src/components/BlogList.jsx
+++ Part-5/blog/bloglist-frontend/src/components/BlogList.jsx
@@ -1,11 +1,17 @@
 import Blog from './Blog/Blog';
 
-const BlogList = ({ blogs, updateBlog, notify }) => {
+const BlogList = ({ blogs, user, modifyBlogs, notify }) => {
   return (
     <div>
       <h2>Blogs</h2>
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} notify={notify}/>
+        <Blog
+          key={blog.id}
+          blog={blog}
+          user={user}
+          modifyBlogs={modifyBlogs}
+          notify={notify}
+        />
       )}
     </div>
   );
