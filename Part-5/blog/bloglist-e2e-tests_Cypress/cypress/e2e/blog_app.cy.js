118992f0ba89ce0c6040ea8d6f5895ee8616f866
Saribeg <saribeg@gmail.com>
2025-07-06 01:31:07 +0400

    part-5/task-5.18-cypress: add: Blog List End To End Testing, step 2

--- Part-5/blog/bloglist-e2e-tests_Cypress/cypress/e2e/blog_app.cy.js
+++ Part-5/blog/bloglist-e2e-tests_Cypress/cypress/e2e/blog_app.cy.js
@@ -1,12 +1,41 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    cy.visit('http://localhost:5173')
-  })
+    cy.request('POST', `${Cypress.env('BACKEND')}/test/initiate-db`);
+    cy.visit('/');
+  });
 
   it('Shows login form when visiting page initially', function() {
     cy.contains('Log In to see blogs and work with them');
     cy.get('#username').should('be.visible');
     cy.get('#password').should('be.visible');
     cy.get('#login').should('be.visible');
+  });
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.fillAndSubmitLoginForm({
+        username: 'harrypotter',
+        password: 'sekret'
+      });
+
+      cy.get('.notification.success')
+        .should('contain', 'User Harry Potter is successfully logged in')
+        .and('have.css', 'color', 'rgb(0, 128, 0)');
+      cy.get('.user').should('be.visible');
+      cy.get('.user-name').should('contain', 'Harry Potter');
+      cy.get('.user-status').should('contain', 'Status: Logged In');
+    });
+
+    it('fails with wrong credentials', function() {
+      cy.fillAndSubmitLoginForm({
+        username: 'harrypotter',
+        password: 'wrong'
+      });
+
+      cy.get('.notification.error')
+        .should('contain', 'invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)');
+      cy.get('.user').should('not.exist');
+    })
   })
-})
+});
