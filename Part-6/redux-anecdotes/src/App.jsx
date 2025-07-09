e74ec17855aa22af1911caa02f69fb1d0bc6301a
Saribeg <saribeg@gmail.com>
2025-07-09 15:52:27 +0400

    part-6/task-6.7: add: Anecdotes with Redux, step 5

--- Part-6/redux-anecdotes/src/App.jsx
+++ Part-6/redux-anecdotes/src/App.jsx
@@ -1,20 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { vote, addAnecdote } from './reducers/anecdoteReducer';
+import { vote } from './reducers/anecdoteReducer';
+import AnecdoteForm from './components/AnecdoteForm';
 
 const App = () => {
   // There are three possible places to handle sorting: in the reducer, in the selector, or in the component.
   // Sorting in the reducer mixes UI logic with state logic, which breaks separation of concerns.
   // Using a selector function is ideal for reuse, but here we sort inline for simplicity and readability,
   // as this is the only place where sorted anecdotes are needed.
-  const anecdotes = useSelector(state => [...state.anecdotes].sort((a, b) => b.votes - a.votes))
+  const anecdotes = useSelector(state => [...state].sort((a, b) => b.votes - a.votes))
   const dispatch = useDispatch()
-
-  const createAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(addAnecdote(content))
-  }
 
   return (
     <div>
@@ -30,11 +24,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={createAnecdote}>
-        <div><input name="anecdote"/></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
