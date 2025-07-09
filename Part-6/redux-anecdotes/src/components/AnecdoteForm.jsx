ad9ae3ee7c3d094af9384c798d555a07b651215f
Saribeg <saribeg@gmail.com>
2025-07-10 01:22:13 +0400

    part-6/task-6.17: add: Anecdotes with Reduxand the Backend, step 4

--- Part-6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ Part-6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,24 +1,22 @@
 
 import { useDispatch } from 'react-redux'
-import { addAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
+import { createAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const createAnecdote = async (event) => {
+  const handleSubmitAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    const newAnecdote = await anecdoteService.createAnecdote(content)
-    dispatch(addAnecdote(newAnecdote))
+    dispatch(createAnecdote(content))
     dispatch(setNotification(`You have added anecdote "${content}"`, 3))
   }
   return (
     <div>
       <h2>create new</h2>
-      <form onSubmit={createAnecdote}>
+      <form onSubmit={handleSubmitAnecdote}>
         <div><input name="anecdote"/></div>
         <button type="submit">create</button>
       </form>
