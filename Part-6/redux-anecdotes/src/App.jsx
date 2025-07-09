8a46971c69d394742b1542b0fb6e29532aaa4f3a
Saribeg <saribeg@gmail.com>
2025-07-09 17:23:50 +0400

    part-6/task-6.9: add: Anecdotes with Redux, step 7

--- Part-6/redux-anecdotes/src/App.jsx
+++ Part-6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,11 @@
-import AnecdoteList from './components/AnecdoteList';
-import AnecdoteForm from './components/AnecdoteForm';
+import Filter from './components/Filter'
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   return (
     <div>
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
