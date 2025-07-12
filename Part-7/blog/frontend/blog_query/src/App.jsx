007990cd3ab7bbff070372af80d7cc3bf6b37c4f
Saribeg <saribeg@gmail.com>
2025-07-12 23:24:21 +0400

    part-7/task-7.11-query: add: Blog, Redux, Step 2

--- Part-7/blog/frontend/blog_query/src/App.jsx
+++ Part-7/blog/frontend/blog_query/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
-import { safeParseJSON, modifyArray } from './utils/commonHelpers';
+import { safeParseJSON } from './utils/commonHelpers';
 import { setToken } from './services/api';
 import LoginForm from './components/LoginForm';
-import blogService from './services/blogs';
 import UserData from './components/UserData';
 import BlogList from './components/BlogList';
 import BlogForm from './components/BlogForm';
@@ -11,18 +10,7 @@ import Togglable from './components/Togglable/Togglable';
 
 const App = () => {
   const [user, setUser] = useState(null);
-  const [blogs, setBlogs] = useState([]);
-
   const blogFormRef = useRef();
-
-  const modifyBlogs = (operationType, blogData) => setBlogs(prevBlogs => modifyArray(prevBlogs, operationType, blogData));
-
-  useEffect(() => {
-    blogService.getAll().then(blogs => {
-      const sorted = [...blogs].sort((a, b) => b.likes - a.likes);
-      setBlogs(sorted);
-    });
-  }, []);
 
   useEffect(() => {
     const userJSON = window.localStorage.getItem('user');
@@ -43,13 +31,10 @@ const App = () => {
           <>
             <UserData user={user} setUser={setUser}/>
             <BlogList
-              blogs={blogs}
               user={user}
-              modifyBlogs={modifyBlogs}
             />
             <Togglable buttonLabel="New Blog" ref={blogFormRef}>
               <BlogForm
-                modifyBlogs={modifyBlogs}
                 toggleForm={() => blogFormRef.current.toggleVisibility()}
               />
             </Togglable>
