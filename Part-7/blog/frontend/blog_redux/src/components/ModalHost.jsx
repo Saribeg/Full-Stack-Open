20f611c5c0f06f24a5f375c9adb0b86026266cb8
Saribeg <saribeg@gmail.com>
2025-07-17 19:00:29 +0400

    Part-7: add: refactor window.comfirm into normal reusable modal window

---
+++ Part-7/blog/frontend/blog_redux/src/components/ModalHost.jsx
@@ -0,0 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux';
+import Modal from './ui/Modal';
+import DeleteBlogModal from './Blog/DeleteBlogModal';
+import { selectModal } from '../store/modal/selectors';
+import { hideModal } from '../store/modal/slice';
+
+const ModalHost = () => {
+  const dispatch = useDispatch();
+  const { isOpened, type, params } = useSelector(selectModal);
+
+  const onClose = () => dispatch(hideModal());
+
+  return (
+    <Modal isOpened={isOpened} onClose={onClose}>
+      {type === 'confirmDelete' && <DeleteBlogModal params={params} />}
+    </Modal>
+  );
+};
+
+export default ModalHost;
