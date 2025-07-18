49b16031fe6d04d2806e3902e14115ff11d51421
Saribeg <saribeg@gmail.com>
2025-07-18 14:42:40 +0400

    Part-7: add: Final changes of redux version

--- Part-7/blog/frontend/blog_redux/src/store/users/selectors.js
+++ Part-7/blog/frontend/blog_redux/src/store/users/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from 'reselect';
+
 export const selectUsersState = (state) => ({
   users: state.users.userList,
   loading: state.users.status.fetch.loading,
@@ -5,6 +7,23 @@ export const selectUsersState = (state) => ({
 });
 
 export const selectAllUsers = (state) => state.users.userList;
+
+export const selectSortedUsers = createSelector([selectAllUsers], (users) =>
+  [...users].sort((a, b) => b.blogs.length - a.blogs.length)
+);
+
+export const selectVisibleUsersState = createSelector(
+  [
+    selectSortedUsers,
+    (state) => state.users.status.fetch.loading,
+    (state) => state.users.status.fetch.error
+  ],
+  (sortedUsers, loading, error) => ({
+    users: sortedUsers,
+    loading,
+    error
+  })
+);
 
 export const selectUserDetailsState = (state) => ({
   user: state.users.selectedUser,
