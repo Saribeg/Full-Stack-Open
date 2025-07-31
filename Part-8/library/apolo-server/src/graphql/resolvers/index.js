a245a8c94c4089d6f7722bc713be7de4d99a5138
Saribeg <saribeg@gmail.com>
2025-07-31 19:47:22 +0400

    part-8/task-8.23: add: GraphQL - Subscriptions - server

--- Part-8/library/apolo-server/src/graphql/resolvers/index.js
+++ Part-8/library/apolo-server/src/graphql/resolvers/index.js
@@ -1,9 +1,11 @@
 const Query = require('./query');
 const Mutation = require('./mutation');
+const Subscription = require('./subscription');
 const Author = require('./author');
 
 module.exports = {
   Query,
   Mutation,
+  Subscription,
   Author
 };
