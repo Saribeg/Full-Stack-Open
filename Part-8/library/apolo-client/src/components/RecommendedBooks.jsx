5c7238db31c2e8156ed2a1588e0aa26d5157902b
Saribeg <saribeg@gmail.com>
2025-07-31 14:00:01 +0400

    part-8/task-8.20: add: GraphQL - Books by genre, part 2

---
+++ Part-8/library/apolo-client/src/components/RecommendedBooks.jsx
@@ -0,0 +1,38 @@
+import { useQuery } from "@apollo/client"
+import { ALL_BOOKS } from '../queries'
+
+const RecommendedBooks = ({ user, show }) => {
+  const { data, loading } = useQuery(ALL_BOOKS, {
+    variables: user ? { genre: user.favoriteGenre } : {},
+  });
+
+  const books = data?.allBooks ?? [];
+
+  if (!show) return null;
+  if (loading) return <div>Loading...</div>;
+
+    return (
+    <div>
+      <h2>recommendations</h2>
+      <p>books in your favourite genre: <b>{user.favoriteGenre}</b></p>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author.name}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default RecommendedBooks;
