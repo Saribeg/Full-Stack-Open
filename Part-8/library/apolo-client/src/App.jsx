4413b7e9969ebccf96025abcbba63b32bb51673d
Saribeg <saribeg@gmail.com>
2025-07-31 12:33:29 +0400

    part-8/task-8.18: add: GraphQL - Log in on client side

--- Part-8/library/apolo-client/src/App.jsx
+++ Part-8/library/apolo-client/src/App.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
+import LoginForm from "./components/LoginForm";
 import NewBook from "./components/NewBook";
+import Logout from "./components/Logout";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [token, setToken] = useState("");
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        {
+          token ? (
+            <>
+              <button onClick={() => setPage("add")}>add book</button>
+              <Logout setToken={setToken} />
+            </>
+          ) : <button onClick={() => setPage("login")}>login</button>
+        }
       </div>
 
-      <Authors show={page === "authors"} />
-
+      <Authors show={page === "authors"} token={token}/>
       <Books show={page === "books"} />
-
+      <LoginForm show={page === "login"} setToken={setToken} setPage={setPage}/>
       <NewBook show={page === "add"} />
     </div>
   );
