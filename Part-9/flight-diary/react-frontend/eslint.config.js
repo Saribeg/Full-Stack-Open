5cb4fd1f7f62d3a4ceeec7aa7bba4d3b82d02427
Saribeg <saribeg@gmail.com>
2025-08-09 15:20:54 +0400

    part-9/task-9.17: add: TypeScript - Flight diaries, part 1

---
+++ Part-9/flight-diary/react-frontend/eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  reactRefresh.configs.vite,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser
+    },
+    settings: { react: { version: 'detect' } },
+    plugins: { react, 'react-hooks': reactHooks },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      eqeqeq: 'error',
+      'no-console': 'warn',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single', { avoidEscape: true }],
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-trailing-spaces': 'error',
+      'linebreak-style': ['error', 'unix'],
+      'comma-dangle': ['error', 'only-multiline'],
+      'indent': ['error', 2, { SwitchCase: 1 }]
+    }
+  }
+]);
