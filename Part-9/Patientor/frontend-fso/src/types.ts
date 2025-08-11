42c8ea322e6f0d12102cfc790c30fd56f7ed9d8d
Saribeg <saribeg@gmail.com>
2025-08-11 18:55:55 +0400

    part-9/task-9.24: add: TypeScript - Patientor, step 4

--- Part-9/Patientor/frontend-fso/src/types.ts
+++ Part-9/Patientor/frontend-fso/src/types.ts
@@ -1,5 +1,4 @@
-export interface Entry {
-}
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
 
 export interface Diagnosis {
   code: string;
@@ -13,6 +12,56 @@ export enum Gender {
   Other = "other"
 }
 
+// Entry
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
+}
+
+interface SickLeave {
+  startDate: string,
+  endDate: string,
+}
+
+interface Discharge {
+  date: string,
+  criteria: string,
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
+
+// Patient
 export interface Patient {
   id: string;
   name: string;
