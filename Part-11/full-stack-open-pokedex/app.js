117bc6fac4e90818557370a03245db4eaa6986c0
Saribeg <saribeg@gmail.com>
2025-08-17 00:32:33 +0400

    part-11/tasks-11.10-11.12: add: Deploy to Fly.io

--- Part-11/full-stack-open-pokedex/app.js
+++ Part-11/full-stack-open-pokedex/app.js
@@ -16,7 +16,5 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('Imitate Error temporarily for checking deployment')
   res.send('ok')
 })
