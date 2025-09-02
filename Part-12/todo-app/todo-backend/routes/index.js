5c7306a68d90a56325a1c6c5606c0e9573af8c04
Saribeg <saribeg@gmail.com>
2025-09-02 21:39:30 +0400

    part-12/task-12.9: add: Set up Redis for the project

--- Part-12/todo-app/todo-backend/routes/index.js
+++ Part-12/todo-app/todo-backend/routes/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const configs = require('../util/config')
+const { getAsync, setAsync } = require('../redis');
 
 let visits = 0
 
 /* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
+  if (getAsync && setAsync) {
+    const cached = await getAsync('visits');
+    const current = Number(cached) || 0;
+    const next = current + 1;
+    await setAsync('visits', next);
+    visits = next;
+  } else {
+    visits++;
+  }
 
   res.send({
     ...configs,
