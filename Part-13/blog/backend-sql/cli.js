2e55eb65dca9c3e8c2c3d3f8a95863d473e1f578
Saribeg <saribeg@gmail.com>
2025-09-13 14:43:26 +0400

    part-13/task-13.3: add: basic table with data

---
+++ Part-13/blog/backend-sql/cli.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const { Sequelize, QueryTypes } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection established');
+    console.log('Executing (default): SELECT * FROM blogs');
+    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    console.log(blogs)
+    sequelize.close()
+  } catch (err) {
+    console.error('Unable to connect to the database:', err);
+    process.exit(1);
+  }
+})();
