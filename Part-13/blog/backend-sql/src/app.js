7af753c560deb8b15df2c6edec9258607d7d8e91
Saribeg <saribeg@gmail.com>
2025-09-16 21:09:01 +0400

    part-13/task-13.16: add: aggregation

--- Part-13/blog/backend-sql/src/app.js
+++ Part-13/blog/backend-sql/src/app.js
@@ -6,6 +6,7 @@ const middleware = require('./utils/middlewares');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
 const blogsRouter = require('./controllers/blogs');
+const authorsRouter = require('./controllers/authors');
 
 const app = express();
 app.use(cors());
@@ -28,6 +29,7 @@ app.get('/health', (req, res) => {
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogsRouter);
+app.use('/api/authors', authorsRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
