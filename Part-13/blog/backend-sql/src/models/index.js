ea025e97dea4bda6467bc74950ce87d1d95a4604
Saribeg <saribeg@gmail.com>
2025-09-16 20:07:20 +0400

    part-13/task-13.12: add: relationships between tables

--- Part-13/blog/backend-sql/src/models/index.js
+++ Part-13/blog/backend-sql/src/models/index.js
@@ -4,6 +4,21 @@ const Blog = require('./blog');
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
+User.addScope('withBlogs', {
+  include: {
+    model: Blog,
+    attributes: { exclude: ['userId'] }
+  }
+});
+
+Blog.addScope('withUserName', {
+  attributes: { exclude: ['userId'] },
+  include: {
+    model: User,
+    attributes: ['name']
+  }
+});
+
 const sync = async () => {
   if (process.env.NODE_ENV === 'development') {
     await User.sync({ alter: true });
