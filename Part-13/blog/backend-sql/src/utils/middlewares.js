961c50af031d9e201208057b90cd9d8b22ae7b53
Saribeg <saribeg@gmail.com>
2025-09-16 19:33:39 +0400

    part-13/task-13.11: add: auth middlewares

--- Part-13/blog/backend-sql/src/utils/middlewares.js
+++ Part-13/blog/backend-sql/src/utils/middlewares.js
@@ -1,5 +1,7 @@
-const { Blog } = require('../models');
+const jwt = require('jsonwebtoken');
 const { ValidationError, UniqueConstraintError, ForeignKeyConstraintError } = require('sequelize');
+const { Blog, User } = require('../models');
+const { SECRET, TEST_SECRET } = require('../utils/config');
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id);
@@ -23,6 +25,9 @@ const errorHandler = (error, req, res, _next) => {
   } else if (error instanceof ForeignKeyConstraintError) {
     statusCode = 400;
     userError = { error: 'invalid foreign key reference' };
+  } else if (error.name === 'JsonWebTokenError') {
+    statusCode = 401;
+    userError = { error: 'token invalid' };
   }
 
   console.error({
@@ -38,8 +43,35 @@ const errorHandler = (error, req, res, _next) => {
   return res.status(statusCode).json(userError);
 };
 
+const userExtractor = async (req, res, next) => {
+  const authorization = req.get('authorization');
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  const decodedToken = jwt.verify(
+    token,
+    (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') ? TEST_SECRET : SECRET
+  );
+
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'token invalid (missing id)' });
+  }
+
+  const user = await User.findByPk(decodedToken.id);
+  if (!user) {
+    return res.status(401).json({ error: 'user not found for token' });
+  }
+
+  req.user = user;
+  next();
+};
+
 module.exports = {
   blogFinder,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  userExtractor
 };
