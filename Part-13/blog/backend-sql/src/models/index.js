d94dfbe4ff6e4c80d5b7942dc6a853c4c44e2c71
Saribeg <saribeg@gmail.com>
2025-09-16 19:33:39 +0400

    part-13/task-13.11: add: auth middlewares

--- Part-13/blog/backend-sql/src/models/index.js
+++ Part-13/blog/backend-sql/src/models/index.js
@@ -1,8 +1,20 @@
 const User = require('./user');
 const Blog = require('./blog');
 
-User.sync();
-Blog.sync();
+User.hasMany(Blog);
+Blog.belongsTo(User);
+
+const sync = async () => {
+  if (process.env.NODE_ENV === 'development') {
+    await User.sync({ alter: true });
+    await Blog.sync({ alter: true });
+  } else {
+    await User.sync();
+    await Blog.sync();
+  }
+};
+
+sync();
 
 module.exports = {
   User,
