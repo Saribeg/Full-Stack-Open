186d371a5ece07d550bc681f7bb7e76be614aee4
Saribeg <saribeg@gmail.com>
2025-09-17 19:18:15 +0400

    part-13/task-13.24: add: disabled to user

--- Part-13/blog/backend-sql/src/models/user.js
+++ Part-13/blog/backend-sql/src/models/user.js
@@ -30,6 +30,10 @@ User.init({
   passwordHash: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  disabled: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
   }
 }, {
   sequelize,
